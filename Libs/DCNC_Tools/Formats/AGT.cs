486a31dd9ac71d2038d0dbd622243678749870d9
ExMex <great.mafia2010@gmail.com>
2017-09-14 04:49:49 +0200

    Re-introduced Exceptions when file chunk is not correct. Reformat AGT

--- Libs/DCNC_Tools/Formats/AGT.cs
+++ Libs/DCNC_Tools/Formats/AGT.cs
@@ -15,23 +15,25 @@ namespace DCNC_Tools.Formats
         private const string FileIdentifier = "NayaPack";
         private const int MaxFileChunkSize = 16384;
         private const int StartFileInfoOffset = 0x20;
-        
-        private int _currentFileInfoOffset = StartFileInfoOffset;
-        private int _currentChunkOffset;
 
-        public int FileCount;
-        public Dictionary<string, byte[]> Files;
-
-        public short VersionMajor;
-        public short VersionMinor;
-
-        public static readonly byte[] XorKey = {
+        public static readonly byte[] XorKey =
+        {
             0x01, 0x05, 0x06, 0x02, 0x04, 0x03, 0x07, 0x08, 0x01, 0x05, 0x06, 0x0f, 0x04, 0x03, 0x07, 0x0c, 0x31, 0x85,
             0x76, 0x39, 0x34, 0x3d, 0x30, 0xe8, 0x67, 0x36, 0x36, 0x32, 0x3e, 0x33, 0x34, 0x3b, 0x11, 0x15, 0x16, 0x16,
             0x14, 0x13, 0x1d, 0x18, 0x11, 0x03, 0x06, 0x0c, 0x04, 0x03, 0x06, 0x08, 0x2e, 0x55, 0x26, 0x23, 0x2a, 0x23,
             0x2e, 0x28, 0x21, 0x21, 0x26, 0x27, 0x2e, 0x00, 0x2d, 0x2d, 0xcf, 0xa5, 0x06, 0x02, 0x04, 0x0f, 0x07, 0x18,
             0xe1, 0x15, 0x36, 0x18, 0x60, 0x13, 0x1a, 0x19, 0x11, 0x15, 0x16, 0x10, 0x12, 0x13, 0x17, 0x38, 0xf1, 0x25
         };
+
+        private int _currentChunkOffset;
+
+        private int _currentFileInfoOffset = StartFileInfoOffset;
+
+        public int FileCount;
+        public Dictionary<string, byte[]> Files;
+
+        public short VersionMajor;
+        public short VersionMinor;
 
         /*
 #if DEBUG
@@ -208,7 +210,7 @@ namespace DCNC_Tools.Formats
         }
         */
         /// <summary>
-        /// Loads the specified AGT/NayaPack file.
+        ///     Loads the specified AGT/NayaPack file.
         /// </summary>
         /// <param name="fileName">Name of the file.</param>
         /// <exception cref="InvalidFileHeaderException">If the fileheader is invalid</exception>
@@ -219,9 +221,10 @@ namespace DCNC_Tools.Formats
         }
 
         /// <summary>
-        /// Loads the specified AGT/NayaPack file.
+        ///     Loads the specified AGT/NayaPack file.
         /// </summary>
         /// <param name="fileName">Name of the file.</param>
+        /// <param name="xorKey">The XOR key to use</param>
         /// <exception cref="InvalidFileHeaderException">If the fileheader is invalid</exception>
         /// <exception cref="System.FormatException">If file inside the AGT was corrupt</exception>
         public void Load(string fileName, byte[] xorKey)
@@ -232,7 +235,7 @@ namespace DCNC_Tools.Formats
                 using (var reader = new XorBinaryReader(ms, xorKey))
                 {
                     var identifier = Encoding.UTF8.GetString(reader.ReadBytes(8));
-                    if(identifier != FileIdentifier)
+                    if (identifier != FileIdentifier)
                         throw new InvalidFileHeaderException();
 
                     reader.ReadInt32(); // Unknown
@@ -241,29 +244,29 @@ namespace DCNC_Tools.Formats
 
                     FileCount = reader.ReadInt32();
                     Files = new Dictionary<string, byte[]>(FileCount);
-                    
+
                     for (var i = 0; i < FileCount; i++)
                     {
                         // Seek to our current file info position
                         reader.BaseStream.Seek(_currentFileInfoOffset, SeekOrigin.Begin);
-                        
+
                         // Get the first chunk position
                         _currentChunkOffset = reader.ReadInt32Xor();
 
                         // Read how many chunks (Max uncompressed size: 16384) this file has
                         var fileChunks = reader.ReadInt32Xor();
-                        
+
                         // Read the total uncompressed file size
                         var fileSize = reader.ReadInt32Xor();
-                        
+
                         // Read filename and add it to our files
                         var currentFileName = reader.ReadStringXor();
                         Files.Add(currentFileName, new byte[fileSize]);
-                        
-                        // Set our current file info position to next file
-                        _currentFileInfoOffset = (int)reader.BaseStream.Position;
 
-                        var currentFileChunkOffset = (fileChunks * 2) + _currentChunkOffset;
+                        // Set our current file info position to next file
+                        _currentFileInfoOffset = (int) reader.BaseStream.Position;
+
+                        var currentFileChunkOffset = fileChunks * 2 + _currentChunkOffset;
                         var totalChunksSizeRead = 0;
                         for (var j = 0; j < fileChunks; j++)
                         {
@@ -272,22 +275,21 @@ namespace DCNC_Tools.Formats
 
                             // Read how big this chunk is
                             var compressedSize = reader.ReadInt16Xor();
-                            
+
                             // Set the current chunk offset position to next chunk
-                            _currentChunkOffset = (int)reader.BaseStream.Position;
-                            
+                            _currentChunkOffset = (int) reader.BaseStream.Position;
+
                             // Seek to the chunk files offset
                             reader.BaseStream.Seek(currentFileChunkOffset, SeekOrigin.Begin);
-                            
+
                             // Set file chunk offset to next file chunk
                             currentFileChunkOffset += compressedSize;
-                            
+
                             // Read the compressed chunk
                             var compressedFile = reader.ReadBytesXor(compressedSize);
 
                             // Weird try catch hack for uncompressed data. (I dunno if that is actually necessary)
                             if (compressedFile[0] == 0x78 && compressedFile[1] >= 0x01 && compressedFile[1] <= 0xDA)
-                            {
                                 try
                                 {
                                     // Decompress the current chunk
@@ -297,36 +299,35 @@ namespace DCNC_Tools.Formats
                                         throw new FormatException(
                                             $"Chunk {j} for file {currentFileName} was too big! Corrupted archive?");
 
-                                    if(j+1 == fileChunks)
-                                        Array.Copy(decompressedFile, 0, Files[currentFileName], totalChunksSizeRead, fileSize - totalChunksSizeRead);
+                                    if (j + 1 == fileChunks)
+                                        Array.Copy(decompressedFile, 0, Files[currentFileName], totalChunksSizeRead,
+                                            fileSize - totalChunksSizeRead);
                                     else
-                                        Array.Copy(decompressedFile, 0, Files[currentFileName], totalChunksSizeRead, decompressedFile.Length);
+                                        Array.Copy(decompressedFile, 0, Files[currentFileName], totalChunksSizeRead,
+                                            decompressedFile.Length);
                                     totalChunksSizeRead += decompressedFile.Length;
                                 }
                                 catch (ZlibException)
                                 {
                                     // Ionic.Zlib had issues with non-compressed files with ZLIB header.
-                                    Debug.WriteLine(@"{0} File is not compressed but tried to decompress (Header: {1:X} {2:X})", currentFileName, compressedFile[0], compressedFile[1]);
-                                    Console.WriteLine(@"{0} File is not compressed but tried to decompress (Header: {1:X} {2:X})", currentFileName, compressedFile[0], compressedFile[1]);
+                                    Debug.WriteLine(
+                                        $@"{currentFileName} File is not compressed but tried to decompress (Header: {
+                                                compressedFile[0]
+                                            :X} {compressedFile[1]:X})");
+                                    Console.WriteLine(
+                                        $@"{currentFileName} File is not compressed but tried to decompress (Header: {
+                                                compressedFile[0]
+                                            :X} {compressedFile[1]:X})");
 
-                                    if (j + 1 == fileChunks)
-                                        Array.Copy(compressedFile, 0, Files[currentFileName], totalChunksSizeRead, fileSize - totalChunksSizeRead);
-                                    else
-                                        Array.Copy(compressedFile, 0, Files[currentFileName], totalChunksSizeRead, compressedFile.Length);
-                                    totalChunksSizeRead += compressedFile.Length;
+                                    throw new Exception(
+                                        $@"{currentFileName} File is not compressed but tried to decompress (Header: {
+                                                compressedFile[0]
+                                            :X} {compressedFile[1]:X})");
                                 }
-                            }
                             else
-                            {
-                                // File should be uncompressed. Just read the "compressedFile" in.
-                                if (j + 1 == fileChunks)
-                                    Array.Copy(compressedFile, 0, Files[currentFileName], totalChunksSizeRead, fileSize - totalChunksSizeRead);
-                                else
-                                    Array.Copy(compressedFile, 0, Files[currentFileName], totalChunksSizeRead, compressedFile.Length);
-                                totalChunksSizeRead += compressedFile.Length;
-                            }
+                                throw new FormatException("Unsupported file!");
                         }
-                        if(Files[currentFileName].Length != fileSize)
+                        if (Files[currentFileName].Length != fileSize)
                             throw new FormatException($"File {currentFileName} corrupt?");
                     }
                 }
